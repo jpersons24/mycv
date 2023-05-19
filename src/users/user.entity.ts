58b535feac0922791b6626892972d83310ed116b
Jakob Persons <jakobep24@gmail.com>
2023-05-19 09:31:17 -0400

    setup route handlers and services for users

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,4 +8,19 @@ export class User {
   email: string;
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('inserted user with id:', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('updated user with id:', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id:', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -10,5 +10,34 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    // fineOneBy returns record that matches data passed into arg OR
+    // returns null if nothing is found
+    return this.repo.findOneBy({ id: id });
+  }
+
+  find(email: string) {
+    // find() returns an array of all records that match the given arg OR
+    // returns an empty array
+    return this.repo.find({ where: { email }});
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found!')
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new NotFoundException('user not found!');
+    }
+    return this.repo.remove(user);
   }
 }

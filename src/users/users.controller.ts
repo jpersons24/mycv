48914d33557be1c6e68282e065603fef0878c43c
Jakob Persons <jakobep24@gmail.com>
2023-05-19 07:00:27 -0400

    format code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,9 @@ import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-
   constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password)
+    this.usersService.create(body.email, body.password);
   }
 }
